0910552191518b3ebecd42c13c4cea6c4ebfe72f
StevenLiu <stevenliu986@gmail.com>
2023-09-05 12:34:18 +1000

    add loading more functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import "./App.css";
 
 type FlightData = {
   flight_number: number;
@@ -12,16 +13,25 @@ type FlightData = {
 
 function App() {
   const [data, setData] = useState<Array<FlightData>>();
+  const [limit, setLimit] = useState(5);
+
+  const loadMore = () => {
+    setLimit((limit) => limit + 5);
+  };
+
+  // const deleteItem = (id: number) => {
+  //   setData(data?.filter(id=> id !== undefined))
+  // }
 
   useEffect(() => {
     axios
-      .get("https://api.spacexdata.com/v3/launches/past")
+      .get(`https://api.spacexdata.com/v3/launches?limit=${limit}`)
       .then((res) => {
         const { data } = res;
         setData(data);
       })
       .catch((err) => console.log(err));
-  }, []);
+  }, [limit]);
 
   if (!data) {
     return <div>Page is Loading...</div>;
@@ -31,14 +41,18 @@ function App() {
     <div className="App">
       {data.map((item) => {
         return (
-          <div key={item.flight_number}>
+          <div key={item.flight_number} className="flight-box">
             <p>{item.flight_number}</p>
             <p>{item.mission_name}</p>
             <p>{item.rocket.rocket_name}</p>
             <p>{item.launch_success ? "success" : "false"}</p>
+            {/* <button onClick={deleteItem}>Delete</button> */}
           </div>
         );
       })}
+      <button onClick={loadMore} className="load-btn">
+        Load More
+      </button>
     </div>
   );
 }
