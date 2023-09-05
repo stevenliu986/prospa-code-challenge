dbcaab13106168b163e912bb2c36f7dfd28180b8
StevenLiu <stevenliu986@gmail.com>
2023-09-05 11:26:43 +1000

    add code for getting data

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+type FlightData = {
+  flight_number: number;
+  mission_name: string;
+  rocket: {
+    rocket_name: string;
+  };
+  launch_success: boolean;
+};
 
 function App() {
+  const [data, setData] = useState<Array<FlightData>>();
+
+  useEffect(() => {
+    axios
+      .get("https://api.spacexdata.com/v3/launches/past")
+      .then((res) => {
+        const { data } = res;
+        setData(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  if (!data) {
+    return <div>Page is Loading...</div>;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data.map((item) => {
+        return (
+          <div key={item.flight_number}>
+            <p>{item.flight_number}</p>
+            <p>{item.mission_name}</p>
+            <p>{item.rocket.rocket_name}</p>
+            <p>{item.launch_success ? "success" : "false"}</p>
+          </div>
+        );
+      })}
     </div>
   );
 }
